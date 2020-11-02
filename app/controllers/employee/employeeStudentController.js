c11b4ac3c63ed15a149a48a302ffc8ea69717e48
Sarath Raj <sarathrj10@gmail.com>
2020-11-02 22:41:12 +0530

    student list added

---
+++ app/controllers/employee/employeeStudentController.js
@@ -0,0 +1,30 @@
+const db = require('../../config/mongoConnection');
+
+function employeeStudentController() {
+  return {
+    students(req, res) {
+      db.get().collection('students').find({}).toArray((err, datas) => {
+        if (err) throw err;
+        res.render('employees/students', { datas });
+      });
+    },
+    addStudent(req, res) {
+      console.log(req.body);
+      const newStudent = req.body;
+      db.get().collection('students').countDocuments({ studentID: newStudent.studentID }, { limit: 1 }, (err, data) => {
+        if (err) throw err;
+        if (data === 1) {
+          req.flash('error', 'Student already exits');
+          res.redirect('/employee/students');
+        } else {
+          db.get().collection('students').insertOne(newStudent, (error) => {
+            if (error) throw error;
+            res.redirect('/employee/students');
+          });
+        }
+      });
+    },
+  };
+}
+
+module.exports = employeeStudentController;
--- routes/employee.js
+++ routes/employee.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const employeeAuthController = require('../app/controllers/employee/employeeAuthController');
 const employeeDashController = require('../app/controllers/employee/employeeDashController');
+const employeeStudentController = require('../app/controllers/employee/employeeStudentController');
 const employeeDashProtect = require('../app/middlewares/employee/employeeDashProtect');
 const employeeAuthProtect = require('../app/middlewares/employee/employeeAuthProtect');
 
@@ -11,11 +12,13 @@ const employeeAuthProtect = require('../app/middlewares/employee/employeeAuthPro
 router.get('/', employeeDashProtect, employeeAuthController().login);
 router.get('/dash', employeeAuthProtect, employeeDashController().dashboard);
 router.get('/otpget', employeeAuthController().otpGet);
+router.get('/students', employeeAuthProtect, employeeStudentController().students);
 router.get('/logout', employeeAuthController().logout);
 
 // postRoutes
 router.post('/', employeeAuthController().postLogin);
 router.post('/otp', employeeAuthController().otpLogin);
 router.post('/otpget', employeeAuthController().otpGetPost);
+router.post('/addStudent', employeeStudentController().addStudent);
 
 module.exports = router;
