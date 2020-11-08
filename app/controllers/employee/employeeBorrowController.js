5538007ecc4d920e45f1f4ab34d706860f305774
Sarath Raj <sarathrj10@gmail.com>
2020-11-08 11:44:10 +0530

    borrow collection compleated

--- app/controllers/employee/employeeBorrowController.js
+++ app/controllers/employee/employeeBorrowController.js
@@ -7,13 +7,13 @@ function employeeBorrowController() {
       res.render('employees/borrow');
     },
     borrowBook(req, res) {
-      const { studentID, ISBN } = req.body;
+      const { studentID, isbn } = req.body;
       function checkStudent(id) {
         new Promise((resolve, reject) => {
           db.get().collection('students').countDocuments({ studentID: id }, { limit: 1 }, (err, data) => {
             if (err) throw err;
             if (data === 1) {
-              resolve(checkBook());
+              resolve(checkBook(isbn));
             } else {
               reject();
             }
@@ -23,12 +23,12 @@ function employeeBorrowController() {
           res.redirect('/employee/borrow');
         });
       }
-      function checkBook() {
+      function checkBook(isbn) {
         new Promise((resolve, reject) => {
-          db.get().collection('books').countDocuments({ ISBN }, { limit: 1 }, (err, data) => {
+          db.get().collection('books').countDocuments({ ISBN: isbn }, { limit: 1 }, (err, data) => {
             if (err) throw err;
             if (data === 1) {
-              resolve(insertToStudent());
+              resolve(checkAndUpdateBookNumber(isbn));
             } else {
               reject();
             }
@@ -38,15 +38,49 @@ function employeeBorrowController() {
           res.redirect('/employee/borrow');
         });
       }
+      function checkAndUpdateBookNumber(isbn) {
+        new Promise((resolve, reject) => {
+          db.get().collection('books').findOne({ ISBN: isbn }, (err, data) => {
+            if (err) throw err;
+            let number = parseInt(data.number, 10);
+            if (number > 0) {
+              number -= 1;
+              db.get().collection('books').updateOne({ ISBN: isbn }, { $set: { number } }, false, (err) => {
+                if (err) throw err;
+                console.log('Book number updated');
+              });
+              resolve(insertToStudent());
+            } else {
+              reject();
+            }
+          });
+        }).catch(() => {
+          req.flash('error', 'The Book is Unavailable right now');
+          res.redirect('/employee/borrow');
+        });
+      }
       function insertToStudent() {
-        const bookTaken = {
-          isbn: ISBN,
-          issue: moment().format('L'),
-          due: moment().add(7, 'days').calendar(),
-        };
-        db.get().collection('students').updateOne({ studentID }, { $set: { bookTaken } }, true, (err) => {
+        new Promise((resolve, reject) => {
+          const bookTaken = {
+            isbn,
+            issue: moment().format('DD/MM/YYYY'),
+            due: moment().add(7, 'days').format('DD/MM/YYYY'),
+          };
+          db.get().collection('students').updateOne({ studentID }, { $set: { bookTaken } }, false, (err) => {
+            if (err) reject();
+            console.log('data inserted to students collection');
+            resolve(insertToBorrow(bookTaken));
+          });
+        }).catch(() => {
+          req.flash('error', 'Something went wrong');
+          res.redirect('/employee/borrow');
+        });
+      }
+      function insertToBorrow(datas) {
+        const details = { ...datas, studentID };
+        db.get().collection('borrow').insertOne(details, (err) => {
           if (err) throw err;
-          console.log('data inserted to students collection');
+          res.redirect('/employee/borrow');
         });
       }
       checkStudent(studentID);
